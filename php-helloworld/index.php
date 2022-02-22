92fce53b131b24f53d1552ccf5cdb9eef935b253
Student User <student@workstation.lab.example.com>
2022-02-21 23:30:59 -0500

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "SECOND LINE ADDED BU SAMIR" . PHP_VERSION . "\n";
+print "\n";
+print "\n";
+print "THIRD LINE ADDED BY SAMIR"  .  "\n";
 ?>
